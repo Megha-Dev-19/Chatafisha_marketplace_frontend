9af0bbd53d21fe91d5a933e99f06a62fe30a884c
NadaLabidi1 <nada.labidi@esprit.tn>
2023-10-30 11:02:38 +0100

    github actions config

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.use(cors());
 app.use(express.json());
