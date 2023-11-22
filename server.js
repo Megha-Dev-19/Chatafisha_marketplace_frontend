9cc50b966d435367253f6f8cc586743bf83fc064
NadaLabidi1 <nada.labidi@esprit.tn>
2023-11-22 10:05:56 +0100

    changed the port to be not read from env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
