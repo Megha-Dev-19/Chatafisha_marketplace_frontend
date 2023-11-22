06f2977b3c84f598ab5c1eab699f413a201b0b6e
NadaLabidi1 <nada.labidi@esprit.tn>
2023-11-22 10:10:29 +0100

    established port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
